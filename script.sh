3eefaa2ff9f5b8861001ec71543f966c8a1d77e2
Bogdan <masterdezign@gmail.com>
2013-10-31 02:22:54 +0100

    Minor

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-echo Welcome! >> welcome.txt
+echo Welcome! > welcome.txt
 echo Please run the next command manually to setup vim  >> welcome.txt
-echo  $ vim +BundleInstall +qall >> welcome.txt
+echo '  $ vim +BundleInstall +qall' >> welcome.txt
 
 sudo apt-get update
