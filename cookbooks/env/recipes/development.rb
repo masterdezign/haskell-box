963c33abfcca9c18c2160a7d5876723ea8ef9257
Bogdan <masterdezign@gmail.com>
2016-03-30 16:26:02 +0200

    Update the development libs

--- cookbooks/env/recipes/development.rb
+++ cookbooks/env/recipes/development.rb
@@ -1,26 +1,25 @@
-%w(vim git zsh exuberant-ctags curl ack-grep).each do |name|
+%w(libgmp-dev freeglut3 freeglut3-dev libcurl4-openssl-dev).each do |name|
   package(name)
 end
 
-# Glasgow Haskell Compiler
-%w(libgmp-dev freeglut3 freeglut3-dev ghc).each do |name|
+%w(curl curl-config git libncurses5-dev libblas-dev liblapack-dev libgsl0-dev neovim htop zsh stack exuberant-ctags ack-grep).each do |name|
   package(name)
 end
-# The next one is done in the postinstall.sh
-# package("haskell-platform")
+
+%w(ipython python-numpy python-scipy python-matplotlib).each do |name|
+  package(name)
+end
 
 conf = { :user => { :name => 'vagrant',
                     :home =>  '/home/vagrant/' } }
 
-setup_vim_cmds = [ "mkdir -p src/vim",
-                   "git clone https://github.com/masterdezign/vim.git src/vim",
-                   "ln -s src/vim/.vimrc .vimrc",
-                   "ln -s src/vim/.vim .vim",
-                   "git clone https://github.com/gmarik/vundle.git .vim/bundle/vundle",
-                   "rm -rf .vim/bundle/snipmate.vim/snippets" ]
+# Setup the environment
+setup_cmds = [ "bash <(curl -sL https://git.io/haskell-vim-now)",
+               "stack install cabal-install",
+               'sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"' ]
 
-setup_vim_cmds.each do |cmd|
-  execute "setup_vim" do
+setup_cmds.each do |cmd|
+  execute "setup_cmds" do
     user conf[:user][:name]
     cwd conf[:user][:home]
     command(cmd)
